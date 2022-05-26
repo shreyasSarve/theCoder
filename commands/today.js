05c00a0ca91963a701853e6824447c89a543b4c4
Shreyas Sarve <i.made.jarvis@gmail.com>
2022-05-27 00:52:47 +0530

    Added no contest embed when asked

--- commands/today.js
+++ commands/today.js
@@ -6,11 +6,29 @@ module.exports.run = (_client, message, args) => {
   console.log(`Iside today-----------`);
   let option = 0;
   if (args.length == 0) {
+    let totalContest = 0;
     for (const platform in platForms) {
       const contest = getTodaysContest(platform);
       if (contest.length == 0) continue;
+      totalContest++;
       console.log("Contests", contest);
       const embed = embedForContest(platform, contest, option);
+      message.channel.send({ embeds: [embed] });
+    }
+    if (totalContest == 0) {
+      const embed = {
+        color: "#00FF00",
+        title: `No Contests Today`,
+        author: {
+          name: `The Coder`,
+        },
+        fields: [],
+        description: "No Contests Today Enjoy ",
+        timestamp: new Date(),
+        footer: {
+          text: "Happy Coding guys !!!!",
+        },
+      };
       message.channel.send({ embeds: [embed] });
     }
   }
